ceb69d1da2310380ff4d1049cb76bcd228a52504
1-21-01-E043-09\POO <santiago.cumbalepn.edu.ec>
2024-07-11 07:17:11 -0500

    xd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,32 @@
 package org.example;
 
+import java.sql.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String url="jdbc:mysql://localhost:3306/EstudiantesA24";
+        String user="root";
+        String password="123456";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String nombre="Santiago Cumbal";
+        String cedula="1753653458";
+        double b1=12.4;
+        double b2=10.4;
+        String sql="INSERT INTO estudiantesA(cedula,nombre,b1,b2) VALUES(?,?,?,?)";
+        try(Connection connection= DriverManager.getConnection(url,user,password)){
+            System.out.println("Conexion exitosa");
+         PreparedStatement cadenaPreparada=connection.prepareStatement(sql);
+         cadenaPreparada.setString(1,cedula);
+         cadenaPreparada.setString(2,nombre);
+         cadenaPreparada.setDouble(3,b1);
+         cadenaPreparada.setDouble(4,b2);
+         cadenaPreparada.executeUpdate();
+
+        }
+        catch(SQLException e){
+            e.printStackTrace();
         }
     }
 }
